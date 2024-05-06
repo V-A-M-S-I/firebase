381a6a49de8ef2ac9ce0846ef11884109e4f66b1
Venkata Vamsi <124290370+V-A-M-S-I@users.noreply.github.com>
2024-05-06 16:36:18 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,14 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth , GoogleAuthProvider } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAldUCFoRa76I-VwACf4O-Qv_9YeaZw7Rw",
-  authDomain: "fir-basics-a6f19.firebaseapp.com",
-  projectId: "fir-basics-a6f19",
-  storageBucket: "fir-basics-a6f19.appspot.com",
-  messagingSenderId: "1032415843577",
-  appId: "1:1032415843577:web:a675f7c3fd002c05da7e28"
-};
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
